b52d0c5aa9708e63a3cc0548dc89719f02cba0dc
AhnafK <akazi1@stuy.edu>
2019-02-06 00:39:40 -0500

    comments

--- 03_canvas-anim/animate.js
+++ 03_canvas-anim/animate.js
@@ -18,25 +18,43 @@ var stop = function(){
 }
 
 var drawDot = function(){
+    //credit to theodore's qaf post
+    window.cancelAnimationFrame(id);
+
+    //clears existing frame
     clear();
+
+    //creates circle
     ctx.beginPath();
     ctx.arc(c.width/2,c.height/2,radius,0,2*Math.PI);
     ctx.stroke();
     ctx.fill();
+
+    //if circle is canvas size, then it will not grow
     if(c.width/2 == radius || c.height/2 == radius){
 	growing = false;
     }
+
+    //if circle is tiny size, then it will not shring
     if(radius == 1){
 	growing = true;
     }
+
+    //If circle grows, then the radius does too
     if(growing){
 	radius+=1;
     }
+    //If circle shrinks, then radius does too
     else{
 	radius+=-1;
     }
+
+    //loop
     id = window.requestAnimationFrame(drawDot);
 }
 
+//Animate buttom listener
 anim.addEventListener("click", drawDot);
+
+//stop listener
 stopbut.addEventListener("click", stop);
